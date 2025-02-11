545e2d4d7bbcfeca363ad1fc120d01d93241b04b
Vishal <vishal.ai23@stellamaryscoe.edu.in>
2025-02-11 14:31:57 +0530

    Update workbook.r

--- workbook.r
+++ workbook.r
@@ -1,3 +1,5 @@
+#Week-01
+
 ID = c(1,2,3,4)
 Name = c("Tim","Ken","John","Elisa")
 Mobile = c("24465","25364","20495","24938")
@@ -67,3 +69,97 @@ fr2 = data.frame("ID" = c(1,2,3,4)
                 "Positions" = c("Vice","Fishman","Captain","Navigator"))
 print(fr1)
 print(fr2)
+
+k=b[3,3]
+print(k)
+
+
+#Matrix concatenation
+c = matrix(c(19,29,39),nrow = 1, ncol = 3, byrow = TRUE)
+# C is a row and for bind b and c
+j=rbind(b,c)
+print(j)
+
+
+
+#Inline function
+func = function(x)x^2+4*x+4
+func(1)
+print(func(1))
+fu9 = func(9)
+print(fu9)
+
+
+#Looping over objects
+l = matrix(1:9,3,3)
+print(l)
+
+#apply func
+print(apply(l,1,sum))# A=mat,1=row wise , 2=clmn wise, sum= func
+print(apply(l,2,sum))
+
+#lapply func
+aslist = list(b,l)
+determinant = lapply(aslist,det)
+print(determinant)
+
+#mapply func(only syntax)
+#ma=mapply(volcylinder,list1,list2)
+
+#tapply func
+li = c(1,1,1,1,2,2,2,3,3)
+vale = c(1,2,3,4,5,6,7,8,9)
+ta = tapply(vale,li,sum)
+print(ta)
+
+#control structures
+i = 16
+if(i>20){
+  print("I is greater than 20")
+}else{
+  
+  print("I is Lesser than 20")
+}
+
+if(i>17){
+  i=i+1
+  print(i)
+}else if(i>18){
+  i=i+2
+  print(i)
+}else{
+  i=i+3
+  print(i)
+}
+
+#sequence func
+print(seq(from=1, to=10, by=3))
+print(seq(from=1, to=10, by=4))
+
+#For loop
+n = 5
+sum = 0
+for(i in seq(1,n,1)){
+  sum = sum+i
+  print(c("for loop:",i,sum))
+}
+
+#DEV
+#Scatter plot
+f = 1:10
+g = f^2
+plot(g)
+
+#Using a dataset mtcars
+plot(mtcars$wt,mtcars$mpg, main = "Scatter plot in r using DS" , xlab = "Weight", ylab = "meter per gallon", pch=19)
+
+#line plot
+plot(f,g,type = 'l')
+plot(mtcars$wt,mtcars$mpg, main = "Scatter plot in r using DS" , xlab = "Weight", ylab = "meter per gallon", pch=19, type='l')
+
+#bar plot
+u = c(24,20,18,27)
+i = c("Jan", "Feb", "Mar", "Apr")
+barplot(u,names.arg = i, xlab="Month",ylab="revenue",col="blue",main="Revenue-bar plot",border="red",)
+
+#Week-02
